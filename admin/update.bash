1d83f345cee58560cd70ed99a23d6f2bd95d2af3
Neeraj Kashyap <nkashy1@gmail.com>
2020-05-29 23:27:15 -0700

    git pull in update script

--- admin/update.bash
+++ admin/update.bash
@@ -5,7 +5,13 @@
 set -e
 
 SCRIPT_DIR=$(realpath $(dirname $0))
+THUMBSUP_DIR=$(dirname "${SCRIPT_DIR}")
 SERVICE_FILE="${SCRIPT_DIR}/thumbsup.service"
+
+echo "Updating Thumbsup codebase at: ${THUMBSUP_DIR}"
+git -C "${THUMBSUP_DIR}" checkout master
+git -C "${THUMBSUP_DIR}" pull
+
 echo "Replacing existing thumbsup service definition with: ${SERVICE_FILE}"
 chmod 644 "${SERVICE_FILE}"
 cp "${SERVICE_FILE}" /etc/systemd/system/thumbsup.service
