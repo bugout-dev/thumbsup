abe2943516308eb02277c2ddd41a5e62744a2add
Neeraj Kashyap <nkashy1@gmail.com>
2020-05-29 23:23:16 -0700

    Realpath to file in update script

--- admin/update.bash
+++ admin/update.bash
@@ -4,11 +4,11 @@
 
 set -e
 
-SCRIPT_DIR=$(dirname $0)
+SCRIPT_DIR=$(realpath $(dirname $0))
 SERVICE_FILE="${SCRIPT_DIR}/thumbsup.service"
 echo "Replacing existing thumbsup service definition with: ${SERVICE_FILE}"
 chmod 644 "${SERVICE_FILE}"
-mv "${SERVICE_FILE}" /etc/systemd/system/thumbsup.service
+cp "${SERVICE_FILE}" /etc/systemd/system/thumbsup.service
 systemctl daemon-reload
 systemctl restart thumbsup.service
 systemctl status thumbsup.service
