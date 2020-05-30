5abb5b49f51d96bebd8e831f06134c2253d27b42
Neeraj Kashyap <nkashy1@gmail.com>
2020-05-29 23:21:45 -0700

    Typo sytemctl -> systemctl

--- admin/update.bash
+++ admin/update.bash
@@ -9,6 +9,6 @@ SERVICE_FILE="${SCRIPT_DIR}/thumbsup.service"
 echo "Replacing existing thumbsup service definition with: ${SERVICE_FILE}"
 chmod 644 "${SERVICE_FILE}"
 mv "${SERVICE_FILE}" /etc/systemd/system/thumbsup.service
-sytemctl daemon-reload
+systemctl daemon-reload
 systemctl restart thumbsup.service
 systemctl status thumbsup.service
