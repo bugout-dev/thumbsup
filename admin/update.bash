5e435362b03c30bde57ce32c9d4d739fbd0fd1fa
Neeraj Kashyap <nkashy1@gmail.com>
2020-05-29 23:21:00 -0700

    Make sure service file has right permissions

--- admin/update.bash
+++ admin/update.bash
@@ -7,6 +7,7 @@ set -e
 SCRIPT_DIR=$(dirname $0)
 SERVICE_FILE="${SCRIPT_DIR}/thumbsup.service"
 echo "Replacing existing thumbsup service definition with: ${SERVICE_FILE}"
+chmod 644 "${SERVICE_FILE}"
 mv "${SERVICE_FILE}" /etc/systemd/system/thumbsup.service
 sytemctl daemon-reload
 systemctl restart thumbsup.service
