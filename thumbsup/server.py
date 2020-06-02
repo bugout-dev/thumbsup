1099f6d418b0abac7c194d4478d6156f336fb399
Neeraj Kashyap <nkashy1@gmail.com>
2020-06-02 11:31:31 -0700

    Allow GET requests against /summary with url param

--- thumbsup/server.py
+++ thumbsup/server.py
@@ -5,6 +5,7 @@ import json
 import logging
 import os
 import time
+from urllib.parse import unquote
 import uuid
 
 from atomicwrites import atomic_write
@@ -22,18 +23,22 @@ QUERIES_DIR = os.environ.get('THUMBSUP_QUERIES_DIR')
 def index():
     return redirect('/index.html')
 
-@app.route('/summary', methods=['POST'])
+@app.route('/summary', methods=['GET', 'POST'])
 def summary():
-    content_type = request.content_type
-    if content_type != 'application/x-www-form-urlencoded':
-        return (
-            f'Invalid content type: {content_type}. '
-            'Use "Content-Type: application/x-www-form-urlencoded"'
-        ), 415
+    if request.method == 'POST':
+        content_type = request.content_type
+        if content_type != 'application/x-www-form-urlencoded':
+            return (
+                f'Invalid content type: {content_type}. '
+                'Use "Content-Type: application/x-www-form-urlencoded"'
+            ), 415
 
-    url = request.form.get('url')
-    if url is None or url == '':
-        return redirect('/index.html')
+        url = request.form.get('url')
+        if url is None or url == '':
+            return redirect('/index.html')
+    else:
+        url_raw = request.args.get('url')
+        url = unquote(url_raw)
 
     if QUERIES_DIR is not None:
         query_time = int(time.time())
